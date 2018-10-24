4a5ee051eea907568884956e26b5b8f15b4503e5
Emanuele <manu969@live.it>
2018-10-24 10:31:47 +0200

    Add files via upload

---
+++ FSM.py
@@ -0,0 +1,19 @@
+from RuleEx import *
+
+
+class FSM:
+    def __init__(self, file_name):
+        self.rules = []
+        self.current_state = "S-Init"
+        f = open(file_name, 'r')
+        f
+        for line in f:
+            line = line.strip()
+            info = line.split(" ")
+            start_state = info[0]
+            end_state = info[1]
+            signal = info[2]
+            action = info[3]
+            new_rule = RuleEx(start_state, end_state, signal, action)
+            self.rules.append(new_rule)
+        f.close()
