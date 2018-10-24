fa2eabec1175c00429733b263bb3a090ad57b8b3
Emanuele <manu969@live.it>
2018-10-24 10:37:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from FSM import *
 
 
 if __name__ == "__main__":
-
+    
+    rows =[]
+    cols = []
+    ledpins = [18, 23, 24]
     fsm = FSM()
-    agent = Agent(fsm, file_name = "password")
+    agent = Agent(fsm, file_name = "password", rows, cols, ledpins)
     agent.run()
